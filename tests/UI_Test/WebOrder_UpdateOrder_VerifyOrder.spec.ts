b9a8f806fcd83c9bb486f7c81d7e3010410fe72e
abdulkareem114593-wq <abdul.kareem114593@gmail.com>
2025-10-16 12:59:31 +0530

    Update for smoke tests done

--- tests/UI_Test/WebOrder_UpdateOrder_VerifyOrder.spec.ts
+++ tests/UI_Test/WebOrder_UpdateOrder_VerifyOrder.spec.ts
@@ -1,7 +1,7 @@
 //import { test, expect } from '@playwright/test';
 import { test, expect } from '@playwright/test';
 
-test('Create Order-Update Order- Verify Order @smoke', async ({ page }) => {
+test('Create Order-Update Order- Verify Order', async ({ page }) => {
   await page.goto('http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx');
   //Browser.object.action
   await page.getByLabel('Username:').fill('Tester');
